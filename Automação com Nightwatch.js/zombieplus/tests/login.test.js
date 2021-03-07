ab11aad51fd63b6ce96a2715c616a1a96c7f7406
Kleber <klebernascimento@outlook.com.br>
2021-03-07 13:06:04 -0300

    Meu primeiro teste com Nightwatch

---
+++ Automação com Nightwatch.js/zombieplus/tests/login.test.js
@@ -0,0 +1,30 @@
+module.exports = {
+    'login com sucesso': (browser) => {
+        browser
+        .resizeWindow('1300', '700')
+        .url('http://zombie-web:5000/login')
+        .waitForElementVisible('.card-login', 3000)
+        .setValue('input[name=email]', 'zumbi@dospalmares.com.br')
+        .setValue('#passId', 'pwd123')
+        .click('.login-button')
+
+        .waitForElementVisible('.user .info span', 3000)
+        .assert.containsText('.user .info span', 'Quilombo')
+        .end();
+    },
+
+    'login com sucesso refatorado': (browser) => {
+        let userInfo = '.user .info span'
+        browser
+        .resizeWindow('1300', '700')
+        .url('http://zombie-web:5000/login')
+        .waitForElementVisible('.card-login', 3000)
+        .setValue('input[name=email]', 'zumbi@dospalmares.com.br')
+        .setValue('#passId', 'pwd123')
+        .click('.login-button')
+
+        .waitForElementVisible(userInfo, 3000)
+        .assert.containsText(userInfo, 'Quilombo')
+        .end();
+    }
+}
