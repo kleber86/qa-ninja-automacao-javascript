3863a36a6f0af423a4c2b9bfcd1d34bb70a9bf40
Kleber <klebernascimento@outlook.com.br>
2021-03-08 10:45:53 -0300

    Múltiplos Steps

--- Automação com Nightwatch.js/zombieplus/tests/login.test.js
+++ Automação com Nightwatch.js/zombieplus/tests/login.test.js
@@ -26,5 +26,28 @@ module.exports = {
         .waitForElementVisible(userInfo, 3000)
         .assert.containsText(userInfo, 'Quilombo')
         .end();
+    },
+    
+    'dado que eu acesso a pagina de login': (browser) => {
+        browser
+        .resizeWindow('1900', '1080')
+        .url('http://zombie-web:5000/login')
+        .waitForElementVisible('.card-login', 3000)
+    },
+
+    'quando eu faço login com sucesso': (browser) => {
+        browser
+        .setValue('input[name=email]', 'zumbi@dospalmares.com.br')
+        .setValue('input[name=password]', 'pwd123')
+        .click('.login-button')
+    },
+
+    'então devo ver o meu nome na area logada': (browser) => {
+        let userInfo = '.user .info span'
+
+        browser
+        .waitForElementVisible(userInfo, 3000)
+        .assert.containsText(userInfo, 'Quilombo')
+        .end();
     }
 }
