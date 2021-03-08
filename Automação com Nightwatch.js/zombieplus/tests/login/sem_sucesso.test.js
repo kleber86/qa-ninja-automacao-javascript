14203bfc1c16bd837eb7d71d78c1092dacc5662d
Kleber <klebernascimento@outlook.com.br>
2021-03-08 14:21:51 -0300

    Fluxos Alternativos com Múltiplos Steps

---
+++ Automação com Nightwatch.js/zombieplus/tests/login/sem_sucesso.test.js
@@ -0,0 +1,47 @@
+module.exports = {
+    'email não informado': (browser) => {
+        let alertInfo = '.alert-info'
+        browser
+            .resizeWindow(1600, 900)
+            .url('http://zombie-web:5000/login')
+            .waitForElementVisible('.card-login')
+            .setValue('#emailId', '')
+            .setValue('#passId', 'pwd123')
+            .click('.login-button')
+            .assert.containsText(alertInfo, 'Opps. Cadê o email?')
+    },
+    'senha não informada': (browser) => {
+        let alertInfo = '.alert-info'
+        browser
+            .resizeWindow(1600, 900)
+            .url('http://zombie-web:5000/login')
+            .waitForElementVisible('.card-login')
+            .setValue('#emailId', 'zumbi@dospalmares.com.br')
+            .setValue('#passId', '')
+            .click('.login-button')
+            .assert.containsText(alertInfo, 'Opps. Cadê a senha?')
+    },
+    'email invalido': (browser) => {
+        let alertDanger = '.alert-danger'
+        browser
+            .resizeWindow(1600, 900)
+            .url('http://zombie-web:5000/login')
+            .waitForElementVisible('.card-login')
+            .setValue('#emailId', '404@hotmail.com')
+            .setValue('#passId', 'pwd123')
+            .click('.login-button')
+            .assert.containsText(alertDanger, 'Usuário e/ou senha inválidos')
+    },
+    'senha invalida': (browser) => {
+        let alertDanger = '.alert-danger'
+        browser
+            .resizeWindow(1600, 900)
+            .url('http://localhost:5000/login')
+            .waitForElementVisible('.card-login')
+            .setValue('#emailId', 'zumbi@dospalmares.com.br')
+            .setValue('#passId', '123')
+            .click('.login-button')
+            .assert.containsText(alertDanger, 'Usuário e/ou senha inválidos')
+            .end();
+    }
+}
