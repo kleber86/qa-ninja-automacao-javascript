3590380f734cbf43b881f36058b65a5ddd663935
Kleber <klebernascimento@outlook.com.br>
2021-03-10 19:03:48 -0300

    Mapeando novos Elementos

---
+++ Automação com Nightwatch.js/zombieplus/pages/movie.js
@@ -0,0 +1,12 @@
+module.exports = {
+    elements:{
+        addButton: '.movie-add',
+        titleInput: 'input[name=title]',
+        statusSelect: 'input[placeholder=Status]',
+        yearInput: 'input[name=year]',
+        dateInput: 'input[name=releaseDate]',
+        castInput: '.cast',
+        plotInput: 'textarea[name=overview]',
+        createButton: '#create-movie'
+    }
+}
