31b6412a2c2a4e9e4a824451139189ec2f2ed135
Kleber <klebernascimento@outlook.com.br>
2021-03-12 12:55:52 -0300

    Bônus: Tricks and Tips

--- Automação com Nightwatch.js/zombieplus/pages/movie.js
+++ Automação com Nightwatch.js/zombieplus/pages/movie.js
@@ -24,9 +24,11 @@ let createActions = {
     },
     uploadCover: function(fileName){
         let fullPath = require('path').resolve(__dirname, '../images/' + fileName)
-        console.log('BUGGGG: ' + fullPath)
-
-        return this.setValue('@uploadInput', fullPath)
+        
+        return this
+            .setValue('@uploadInput', fullPath)
+            .pause(1000)
+            .assert.attributeContains('.picture-src', 'src', 'blob')
     }
 }
 
